e48d6b22235efd6cbbbce58934cd87d154eaf9f1
Tolga Cankaya <cankaya.tolga@outlook.com>
2023-06-20 16:52:39 +0300

    second commit

---
+++ src/components/footerend.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function FooterEnd() {
+  return (
+    <div>
+      <footer class="info">
+	<p>Click to edit a todo</p>
+	<p>Created by <a href="https://github.com/Eda-Inal">Eda Inal</a></p>
+	<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+</footer>
+    </div>
+  )
+}
+
+export default FooterEnd
